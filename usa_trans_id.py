83e5d1098e9d34f6735f85b4af7f52b30f7dee9b
Simon Tegg <admin@macbook-pro.lan>
2022-10-29 15:24:52 +1300

    init

---
+++ usa_trans_id.py
@@ -0,0 +1,93 @@
+from datetime import date
+
+import pandas as pd
+import dataframe_image as dfi
+
+# source_name = 'Household Pulse Survey PUF: October 5 - October 17'
+# source_url = 'https://www.census.gov/programs-surveys/household-pulse-survey/datasets.html'
+# source_start = '2022-10-05'
+# source_end = '2022-10-17'
+country = 'usa'
+source = 'pulse2022_puf_50.csv'
+year = date.today().year
+statistic = "trans_id"
+name = f"{country}_{statistic}_{year}"
+
+usa = pd.read_csv(f"./data/{source}")
+
+age_groups = [
+        (18, 24),
+        (25, 29),
+        (30, 34),
+        (35, 39),
+        (40, 44),
+        (45, 49),
+        (50, 54),
+        (55, 59),
+        (60, 64),
+        (65, 88), #1934 is the earliest year in HPS
+        ]
+
+def n(df):
+    return df.shape[0]
+
+
+index = []
+stats = []
+for i, age_group in enumerate(age_groups):
+    lower = age_group[0]
+    upper = age_group[1]
+    year_max = year - lower
+    year_min = year - upper
+    age_query = f"TBIRTH_YEAR >= {year_min} and TBIRTH_YEAR <= {year_max}"
+
+    by_age =    usa.query(age_query)
+    total =     n(by_age)
+    
+    males =     by_age.query("EGENID_BIRTH == 1")
+    m_total =   n(males)
+    m_f =       males.query("GENID_DESCRIBE == 2")
+    m_trans =   males.query('GENID_DESCRIBE == 3')
+    m_none =    males.query('GENID_DESCRIBE == 4')
+
+    females =   by_age.query("EGENID_BIRTH == 2")
+    f_total =   n(females)
+    f_m =       females.query("GENID_DESCRIBE == 1")
+    f_trans =   females.query("GENID_DESCRIBE == 3")
+    f_none =    females.query("GENID_DESCRIBE == 4")
+
+    row = {
+            "m_f": n(m_f) / m_total,
+            "m_trans": n(m_trans) / m_total,
+            "m_none": n(m_none) / m_total,
+            "m_f_trans": (n(m_f) + n(m_trans)) / m_total,
+            "m_trans_none": (n(m_trans) + n(m_none)) / m_total,
+            "m_all": (n(m_f) + n(m_trans) + n(m_none)) / m_total,
+            "f_m": n(f_m) / f_total,
+            "f_trans": n(f_trans) / f_total,
+            "f_none": n(f_none) / f_total,
+            "f_m_trans": (n(f_m) + n(f_trans)) / f_total,
+            "f_trans_none": (n(f_trans) + n(f_none)) / f_total,
+            "f_all": (n(f_m) + n(f_trans) + n(f_none)) / f_total,
+            "opposite_sex_id": (n(f_m) + n(m_f)) / total,
+            "trans_nb": (n(f_m) + n(m_f) + n(m_trans) + n(f_trans) + n(m_none) + n(f_none)) / total,
+            }
+
+    index.append(f"{lower}_{upper}")
+    stats.append(row)
+
+
+
+df = pd.DataFrame(stats, index=index)
+df.to_csv(f"./results/{name}.csv", float_format="%.4f")
+
+# Table png
+percent = lambda x: "{:.2f}%".format(x*100)
+styled = df.style.format(percent)
+dfi.export(styled, f"results/{name}.png")
+
+
+
+
+
+
