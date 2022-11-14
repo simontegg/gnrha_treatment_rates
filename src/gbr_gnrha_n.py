fd5ca606d65cf9e8dcb20e5c5537dea7a6797c02
Simon Tegg <admin@macbook-pro.lan>
2022-11-15 08:08:50 +1300

    update

--- src/gbr_gnrha_n.py
+++ src/gbr_gnrha_n.py
@@ -17,11 +17,15 @@ did_not_start_gnrha = ["A2", "A5", "A7", "A8", "B1", "C1"]
 started = started_gnrha + possibly_started_gnrha
 
 cond = lt_18["category"].isin(started)
+cond2 = lt_18["category"].isin(started_gnrha)
 gnrha_lt_18 = lt_18[cond]
+gnrha_lt_18_alt = lt_18[cond2]
 
 total = referred["n"].sum()
 gnrha_lt_18_sum = gnrha_lt_18["n"].sum()
+gnrha_lt_18_sum_alt = gnrha_lt_18_alt["n"].sum()
 
 print(f"total referred to endocrinology: {total}")
 print(f"total recieved gnrha and <18 at referral: {gnrha_lt_18_sum}")
+print(f"total definitely recieved gnrha and <18 at referral: {gnrha_lt_18_sum_alt}")
 
