66ab6636e61b7d8bee59885ba181db0b8f69ae19
Simon Tegg <simon@simontegg.com>
2024-03-05 22:32:00 +1300

    2024

--- src/nzl_gnrha_rate_2024.py
+++ src/nzl_gnrha_rate_2024.py
@@ -12,7 +12,7 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 statistic = "gnrha_rate"
-years = "2011_2023"
+years = "2009_2023"
 country = "nzl"
 name = f"latest_{country}_{statistic}_{years}"
 
@@ -26,7 +26,6 @@ gnrha = pd.read_excel(
         skiprows=15, 
         header=0,
     ) 
-
 
 df = gnrha.drop(gnrha.columns[0], axis=1)
 
@@ -44,7 +43,6 @@ inc_0_17.metadata_name = "inc_0_17"
 inc_12_17 = df.iloc[:, 16:]
 inc_12_17.columns = columns_12_17
 inc_12_17.metadata_name = "inc_12_17"
-
 
 male_q = "Gender == 'Male'"
 female_q = "Gender == 'Female'"
@@ -92,13 +90,17 @@ prev = pd.DataFrame({
         "females_0_11": dfs["prev_0_17_female"]["Patients"] - dfs["prev_12_17_female"]["Patients"],
     }, index=y_years)
 
+prev["total_12_17"] = prev["males_12_17"] + prev["females_12_17"]
+prev["total_0_11"] = prev["males_0_11"] + prev["females_0_11"]
+
 carryover = pd.DataFrame({
         'year': y_years,
         "males": prev["males_12_17"] - inc["males_12_17"],
         "females": prev["females_12_17"] - inc["females_12_17"],
     })
 
-durations = pd.DataFrame({
+
+m_durations = pd.DataFrame({
         'year': y_years,
         "males_12_17_prev": prev["males_12_17"],
         "males_12_17_prev_2": inc["males_12_17"].rolling(window=2, min_periods=1).sum(),
@@ -107,183 +109,147 @@ durations = pd.DataFrame({
         "males_12_17_prev_5": inc["males_12_17"].rolling(window=5, min_periods=1).sum(),
     })
 
+m_durations_0_11 = pd.DataFrame({
+        'year': y_years,
+        "males_0_11_prev": prev["males_0_11"],
+        "males_0_11_prev_2": inc["males_0_11"].rolling(window=2, min_periods=1).sum(),
+        "males_0_11_prev_3": inc["males_0_11"].rolling(window=3, min_periods=1).sum(),
+        "males_0_11_prev_4": inc["males_0_11"].rolling(window=4, min_periods=1).sum(),
+        "males_0_11_prev_5": inc["males_0_11"].rolling(window=5, min_periods=1).sum(),
+    })
+
+f_durations = pd.DataFrame({
+        'year': y_years,
+        "females_12_17_prev": prev["females_12_17"],
+        "females_12_17_prev_2": inc["females_12_17"].rolling(window=2, min_periods=1).sum(),
+        "females_12_17_prev_3": inc["females_12_17"].rolling(window=3, min_periods=1).sum(),
+        "females_12_17_prev_4": inc["females_12_17"].rolling(window=4, min_periods=1).sum(),
+        "females_12_17_prev_5": inc["females_12_17"].rolling(window=5, min_periods=1).sum(),
+    })
+
+f_durations_0_11 = pd.DataFrame({
+        'year': y_years,
+        "females_0_11_prev": prev["females_0_11"],
+        "females_0_11_prev_2": inc["females_0_11"].rolling(window=2, min_periods=1).sum(),
+        "females_0_11_prev_3": inc["females_0_11"].rolling(window=3, min_periods=1).sum(),
+        "females_0_11_prev_4": inc["females_0_11"].rolling(window=4, min_periods=1).sum(),
+        "females_0_11_prev_5": inc["females_0_11"].rolling(window=5, min_periods=1).sum(),
+    })
 
 
-
-print(durations)
-durations.plot(kind="line", x="year")
+print(inc)
+# m_durations_0_11.plot(kind="line", x="year")
 
 seaborn.set_theme()
-colors = seaborn.color_palette("bright")
-colors.reverse()
+
+total_c = seaborn.color_palette("dark")[7]
+f_c = seaborn.color_palette()[3]
+m_c = seaborn.color_palette()[0]
 
 
 # # # Fig 1 Incidence
-# seaborn.lineplot(x="year", y='females_12_17', data=inc, color=colors[0], linestyle="solid", label="F 12-17")
-# seaborn.lineplot(x="year", y='males_12_17', data=inc, color=colors[3], linestyle="solid", label="M 12-17")
-# seaborn.lineplot(x="year", y='females_0_11', data=inc, color=colors[2], linestyle="solid", label="F 0-11")
-# seaborn.lineplot(x="year", y='males_0_11', data=inc, color=colors[4], linestyle="solid", label="M 0-11")
+# seaborn.lineplot(x="year", y='females_12_17', data=inc, color=f_c, linestyle="solid", label="F 12-17")
+# seaborn.lineplot(x="year", y='females_0_11', data=inc, color=f_c, linestyle="solid", label="F 0-11")
+# seaborn.lineplot(x="year", y='males_12_17', data=inc, color=m_c, linestyle="solid", label="M 12-17")
+# seaborn.lineplot(x="year", y='males_0_11', data=inc, color=m_c, linestyle="solid", label="M 0-11")
 # plt.ylim([0, 120])
 # plt.yticks(ticks=[20, 40, 60, 80, 100, 120])
+# plt.ylabel("Treatment incidence")
+
+# Prevalence
+# seaborn.lineplot(x="year", y='total_12_17', data=prev, color=total_c, linestyle="solid", label="Total 12-17")
+# seaborn.lineplot(x="year", y='total_0_11', data=prev, color=total_c, linestyle="dashed", label="Total 0-11")
+# seaborn.lineplot(x="year", y='females_12_17', data=prev, color=f_c, linestyle="solid", label="F 12-17")
+# seaborn.lineplot(x="year", y='females_0_11', data=prev, color=f_c, linestyle="dashed", label="F 0-11")
+# seaborn.lineplot(x="year", y='males_12_17', data=prev, color=m_c, linestyle="solid", label="M 12-17")
+# seaborn.lineplot(x="year", y='males_0_11', data=prev, color=m_c, linestyle="dashed", label="M 0-11")
+# plt.ylim([0, 450])
+# plt.yticks(ticks=[50, 100, 150, 200, 250, 300, 350, 400, 450])
+# plt.ylabel("Treatment prevalence")
 
 
-# seaborn.lineplot(x="year", y='females_12_17', data=inc, color=colors[0], linestyle="solid", label="F 12-17")
-# seaborn.lineplot(x="year", y='males_12_17', data=inc, color=colors[3], linestyle="solid", label="M 12-17")
-# seaborn.lineplot(x="year", y='females_0_11', data=inc, color=colors[2], linestyle="solid", label="F 0-11")
-# seaborn.lineplot(x="year", y='males_0_11', data=inc, color=colors[4], linestyle="solid", label="M 0-11")
-# plt.ylim([0, 120])
-# plt.yticks(ticks=[20, 40, 60, 80, 100, 120])
-
-
+# General
 # ticks = [f"{x}-01-1" for x in range(2006, 2025, 2)]
 # labels = [f"{x}" for x in range(2006, 2025, 2)]
-
 # plt.xticks(ticks=ticks, labels=labels)
 # plt.rc('font', size=12)  
-# plt.ylabel("Treatment incidence")
 # plt.xlabel(None)
 # plt.xlim(["2006-01-01", "2024-01-01"])
 # plt.legend(loc='upper left')
+# plt.show()
 
 
-plt.show()
+pop = pd.read_csv(
+        f"./data/{pop_source}",
+        skiprows=2, 
+        skipfooter=48,
+        index_col=0,
+        header=None,
+        engine="python"
+        )
+
+def to_int(years):
+    return int(years.replace(" Years", ""))
+
+pop = pop.fillna(method="ffill", axis=1)
+sex_age = [pop.iloc[0].tolist(), map(to_int, pop.iloc[1].tolist())]
+cols = list(zip(*sex_age))
+index_cols = pd.MultiIndex.from_tuples(cols, names=['sex', 'age'])
+
+pop.index = pop.index.map(lambda x: f"{x}-01-01")
+pop.columns = index_cols
+pop = pop.iloc[2:]
+pop.sort_index(axis=1, inplace=True, ascending=True) 
+pop.sort_index(inplace=True, ascending=True) 
+pop = pop.astype(int)
 
 
-# total_0_17 = sum(df['Patients'] for df in dfs_0_17)
-# total_12_17 = sum(df['Patients'] for df in dfs_12_17)
+female_pop_0_17 = pop.loc[:, ("Female", slice(None))]
+male_pop_0_17 = pop.loc[:, ("Male", slice(None))]
+pop_0_17 = pop.loc[:, ("Total", slice(None))]
 
-# # print(females_12_17)
-# # print(males_12_17)
+female_pop_0_17.columns = female_pop_0_17.columns.droplevel()
+male_pop_0_17.columns = male_pop_0_17.columns.droplevel()
+pop_0_17.columns = pop_0_17.columns.droplevel()
 
+female_pop_12_17 = female_pop_0_17.loc[:, 12:17]
+male_pop_12_17 = male_pop_0_17.loc[:, 12:17]
+pop_12_17 = pop_0_17.loc[:, 12:17]
+pop_6_17 = pop_0_17.loc[:, 6:17]
+pop_12_17.index = y_years
+pop_6_17.index = y_years
 
+df = pd.DataFrame({
+    'year': y_years,
+    'males_0_11': inc["males_0_11"],
+    'females_0_11': inc["females_0_11"],
+    'total_0_11': inc["males_0_11"] + inc["females_0_11"],
+    'males_12_17': inc["males_12_17"],
+    'females_12_17': inc["females_12_17"],
+    'total_12_17': inc['males_12_17'] + inc['females_12_17'],
+    })
 
-
-# # males = males.drop(labels=[31, 33])
-# # females = females.drop(labels=[30, 32])
-# # males = males.reset_index(drop=True)
-# # females = females.reset_index(drop=True)
-
-
-# # males = males.reindex(labels=mapper)
-# # females.rename(index=mapper, inplace=True)
-
-
-# ## Change back to 2021 max for comparison
-# # males.index = pd.to_datetime([f"{y}-01-01" for y in range(2006, 2021)])
-# # m.index = pd.to_datetime([f"{y}-01-01" for y in range(2006, 2023)])
-# # females.index = pd.to_datetime([f"{y}-01-01" for y in range(2006, 2021)])
-# # f.index = pd.to_datetime([f"{y}-01-01" for y in range(2006, 2023)])
-# # y_years = pd.to_datetime([f"{y}-01-01" for y in range(2006, 2021)])
-
-# pop = pd.read_csv(
-#         f"./data/{pop_source}",
-#         skiprows=2, 
-#         skipfooter=48,
-#         index_col=0,
-#         header=None,
-#         engine="python"
-#         )
+df["gd_cumulative_incidence"] = df["total_12_17"]["2010-01-01":].cumsum()
 
 
-# def to_int(years):
-#     return int(years.replace(" Years", ""))
-
-# pop = pop.fillna(method="ffill", axis=1)
-# sex_age = [pop.iloc[0].tolist(), map(to_int, pop.iloc[1].tolist())]
-# cols = list(zip(*sex_age))
-# index_cols = pd.MultiIndex.from_tuples(cols, names=['sex', 'age'])
-
-# pop.index = pop.index.map(lambda x: f"{x}-01-01")
-# pop.columns = index_cols
-# pop = pop.iloc[2:]
-# pop.sort_index(axis=1, inplace=True, ascending=True) 
-# pop.sort_index(inplace=True, ascending=True) 
-# pop = pop.astype(int)
-
-# # drop 2023
-# last_row_index = pop.index[-1]
-# pop = pop.drop(last_row_index)
+df["pop_12_17"] = pop_12_17.sum(axis=1)
+df['pop_6_17'] = pop_6_17.sum(axis=1)
+df['pop_period_start_2008'] = df.loc["2008-01-01", "pop_12_17"]
+df['pop_period_start_2009'] = df.loc["2009-01-01", "pop_12_17"]
+df['pop_period_start_2016'] = df.loc["2016-01-01", "pop_12_17"]
+df['pop_period_start_2017'] = df.loc["2017-01-01", "pop_12_17"]
+df['pop_period_start_6_17_2017'] = df.loc["2017-01-01", "pop_6_17"]
 
 
+df['cum_inc_per_12_17_100k_2008'] = (df['gd_cumulative_incidence'] / df['pop_period_start_2008']) * 100000
+df['cum_inc_per_12_17_100k_2009'] = (df['gd_cumulative_incidence'] / df['pop_period_start_2009']) * 100000
+df['cum_inc_2017_2021'] = df.loc['2017-01-01':'2021-01-01', 'gd_cumulative_incidence'].cumsum()
+df['cum_inc_per_12_17_100k_2017'] = (df['cum_inc_2017_2021'] / df['pop_period_start_2017']) * 100000
 
 
-# female_pop_0_17 = pop.loc[:, ("Female", slice(None))]
-# male_pop_0_17 = pop.loc[:, ("Male", slice(None))]
-# pop_0_17 = pop.loc[:, ("Total", slice(None))]
+df.to_csv(f"./results/{name}.csv", float_format="%.2f")
 
-# female_pop_0_17.columns = female_pop_0_17.columns.droplevel()
-# male_pop_0_17.columns = male_pop_0_17.columns.droplevel()
-# pop_0_17.columns = pop_0_17.columns.droplevel()
-
-# female_pop_12_17 = female_pop_0_17.loc[:, 12:17]
-# male_pop_12_17 = male_pop_0_17.loc[:, 12:17]
-# pop_12_17 = pop_0_17.loc[:, 12:17]
-# pop_12_17.index = y_years
-
-# # pop_12_17['total'] = pop_12_17.sum(axis=1)
-
-# # pop_12_17_ = pd.concat([pop_12_17, pd.DataFrame([[np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]], columns=pop_12_17.columns)], ignore_index=True)
-# # pop_12_17_ = pd.concat([pop_12_17_, pd.DataFrame([[np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]], columns=pop_12_17.columns)], ignore_index=True)
-
-# # pop_12_17_.index = y_years
-# # pop_12_17_.interpolate(axis=0, inplace=True, method='time')
-
-# def incidence_from_prevalence(series, window):
-#     incidences = []
-#     for i, prevalence in enumerate(series): 
-#         if i > (window - 2):
-#             incidence = prevalence - sum([incidences[i-j] for j in range(1, window)])
-#             incidences.append(incidence)
-#         else:
-#             incidences.append(prevalence)
-
-#     return incidences
-
-# df = pd.DataFrame({
-#     'year': y_years,
-#     'males_0_17': males_0_17["Patients"],
-#     'unknown_0_17': summed_other["Patients"],
-#     'females_0_17': females_0_17["Patients"],
-#     'males_12_17': males_12_17["Patients"],
-#     'females_12_17': females_12_17["Patients"],
-#     'gnrha_prevalence_12_17': total_12_17, 
-#     'gnrha_prevalence_0_17': total_0_17
-#     })
-
-# df["males_0_11"] = df["males_0_17"] - df["males_12_17"]
-# df["females_0_11"] = df["females_0_17"] - df["females_12_17"]
-# df['total_0_11'] = df["males_0_11"] + df["females_0_11"]
-# df["gnrha_prevalence_GD_12_17"] = df["gnrha_prevalence_12_17"]
-# cutoff_date = pd.Timestamp('2010-01-01')
-# df.loc[df.index < cutoff_date, 'gnrha_prevalence_GD_12_17'] = 0
-
-# df['3yr_duration_incidence'] = incidence_from_prevalence(df['gnrha_prevalence_GD_12_17'], 3)
-# df['4yr_duration_incidence'] = incidence_from_prevalence(df['gnrha_prevalence_GD_12_17'], 4)
-# df['5yr_duration_incidence'] = incidence_from_prevalence(df['gnrha_prevalence_GD_12_17'], 5)
-# df['cumsum_3'] = df['3yr_duration_incidence'].cumsum()
-# df['cumsum_4'] = df['4yr_duration_incidence'].cumsum()
-# df['cumsum_5'] = df['5yr_duration_incidence'].cumsum()
-# df["pop_12_17"] = pop_12_17.sum(axis=1)
-# df['pop_6_17'] = pop_0_17.loc[:, 6:17].sum(axis=1)
-# df['pop_period_start_2008'] = df.loc["2008-01-01", "pop_12_17"]
-# df['pop_period_start_2009'] = df.loc["2009-01-01", "pop_12_17"]
-# df['pop_period_start_2016'] = df.loc["2016-01-01", "pop_12_17"]
-# df['pop_period_start_2017'] = df.loc["2017-01-01", "pop_12_17"]
-# df['pop_period_start_6_17_2017'] = df.loc["2017-01-01", "pop_6_17"]
-
-# df['cum_3yr_inc_per_12_17_100k_2008'] = (df['cumsum_3'] / df['pop_period_start_2008']) * 100000
-# df['cum_3yr_inc_per_12_17_100k_2009'] = (df['cumsum_3'] / df['pop_period_start_2009']) * 100000
-# df['cum_4yr_inc_per_12_17_100k_2008'] = (df['cumsum_4'] / df['pop_period_start_2008']) * 100000
-# df['cum_4yr_inc_per_12_17_100k_2009'] = (df['cumsum_4'] / df['pop_period_start_2009']) * 100000
-
-
-# df['cumsum_3_2017_2021'] = df.loc['2016-01-01':'2020-01-01', '3yr_duration_incidence'].cumsum()
-
-# df['cum_3yr_inc_per_12_17_100k_2017'] = (df['cumsum_3_2017_2021'] / df['pop_period_start_2017']) * 100000
-
-
-# df.to_csv(f"./results/{name}.csv", float_format="%.2f")
-
+print(df)
 
 # print(df)
 
